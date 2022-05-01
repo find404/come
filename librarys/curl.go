16e166a0372412859e9a7c62a637f9285ab2a146
zhy <find404@foxmail.com>
2022-05-01 21:15:07 +0800

    replace curl func

--- librarys/curl.go
+++ librarys/curl.go
@@ -7,68 +7,95 @@ package librarys
  * @version     0.0.1 版本号
  */
 import (
-	"encoding/json"
-	"fmt"
+	"crypto/tls"
+	"errors"
 	"io"
-	"io/ioutil"
 	"net/http"
 )
 
-type HttpRequest struct {
-	Scheme             string
-	Host               string
-	Mothod             string
-	Url                string
-	ResponseStatusCode int
-	HeadersMap         map[string]string
-	RequestJsonBytes   io.Reader
-}
+var httpClient = http.Client{Transport: &http.Transport{
+	TLSClientConfig: &tls.Config{
+		InsecureSkipVerify: true,
+	},
+}}
 
-// 发起请求
-func (hr HttpRequest) GetResponse(bodyStruct interface{}) {
-	request, err := http.NewRequest(hr.Mothod, hr.Url, hr.RequestJsonBytes)
-	request.Host = hr.HeadersMap["host"]
-	for key, header := range hr.HeadersMap {
-		request.Header.Set(key, header)
+// GetResponseInfo 发起请求，返回repson
+func GetResponseInfo(mothod, URL string, header map[string][]string, requestBytes io.Reader) (*http.Response, error) {
+	var result *http.Response
+	var infoError error
+	if mothod == ClEmpty {
+		mothod = http.MethodGet
+	}
+	if URL == ClEmpty {
+		infoError = errors.New("URL is empty")
+		return result, infoError
+	}
+	if header == nil {
+		infoError = errors.New("header is empty")
+		return result, infoError
+	}
+	request, newRequestError := http.NewRequest(mothod, URL, requestBytes)
+	if newRequestError != nil {
+		infoError = newRequestError
+		return result, infoError
+	}
+	request.Header = header
+	response, clientDoError := httpClient.Do(request)
+	if clientDoError != nil {
+		infoError = clientDoError
+		return result, infoError
 	}
 
-	client := &http.Client{}
-	response, err := client.Do(request)
-	if err != nil {
-		fmt.Println("client.Do err : %v", err)
-		return
+	result = response
+	infoError = nil
+	return result, infoError
+}
+
+// GetBody 发起请求，返回body
+func GetBody(mothod, URL string, header map[string][]string, requestBytes io.Reader) ([]byte, error) {
+	var bodyByte []byte
+	var bodyError error
+	responseInfo, responseInfoError := GetResponseInfo(mothod, URL, header, requestBytes)
+	if responseInfoError != nil {
+		bodyError = responseInfoError
+		return bodyByte, bodyError
 	}
-
-	defer response.Body.Close()
-
-	if hr.ResponseStatusCode == 0 {
-		hr.ResponseStatusCode = http.StatusOK
+	readAllBody, readAllError := io.ReadAll(responseInfo.Body)
+	responseBodyError := responseInfo.Body.Close()
+	if responseBodyError != nil {
+		bodyError = responseBodyError
+		return bodyByte, bodyError
 	}
-
-	if response.StatusCode == hr.ResponseStatusCode {
-		body, err := ioutil.ReadAll(response.Body)
-		if err == nil {
-			json.Unmarshal(body, &bodyStruct)
-		}
+	if readAllError != nil {
+		bodyError = readAllError
+		return bodyByte, bodyError
 	}
+	bodyByte = readAllBody
+	bodyError = nil
+	return bodyByte, bodyError
 }
 
-//设置get请求
-func (hr *HttpRequest) SetMothodGet() {
-	hr.Mothod = http.MethodGet
-}
-
-//设置post请求
-func (hr *HttpRequest) SetMothodPost() {
-	hr.Mothod = http.MethodPost
-}
-
-//设置状态码请求
-func (hr *HttpRequest) SetResponseStatusCode200() {
-	hr.ResponseStatusCode = http.StatusOK
-}
-
-//设置状态码请求
-func (hr *HttpRequest) SetResponseStatusCode302() {
-	hr.ResponseStatusCode = http.StatusFound
+// GetResponseBody 发起请求，返回Response body error
+func GetResponseBody(mothod, URL string, header map[string][]string, requestBytes io.Reader) (*http.Response, []byte, error) {
+	var result *http.Response
+	var bodyByte []byte
+	var bodyError error
+	responseInfo, responseInfoError := GetResponseInfo(mothod, URL, header, requestBytes)
+	if responseInfoError != nil {
+		bodyError = responseInfoError
+		return result, bodyByte, bodyError
+	}
+	readAllBody, readAllError := io.ReadAll(responseInfo.Body)
+	responseBodyError := responseInfo.Body.Close()
+	if responseBodyError != nil {
+		bodyError = responseBodyError
+		return result, bodyByte, bodyError
+	}
+	if readAllError != nil {
+		bodyError = readAllError
+		return result, bodyByte, bodyError
+	}
+	bodyByte = readAllBody
+	bodyError = nil
+	return responseInfo, bodyByte, bodyError
 }
